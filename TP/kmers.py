f9b1f9d3fcf74a4a445cefc430881b1dd76fd953
Tristan Hacquard <tristan.hacquard@polytechnique.org>
2024-10-03 17:24:52 +0200

    feature: added kmer constution

--- TP/kmers.py
+++ TP/kmers.py
@@ -14,7 +14,29 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
+def get_id_nucleotide(s:str):
+    if s == 'A':
+        return 0b00
+    if s == 'T':
+        return 0b11
+    if s == 'G':
+        return 0b01
+    if s == 'C':
+        return 0b10
+def get_id_rev(s:str):
+    return (~get_id_nucleotide(s))&0b11
 
-def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+def stream_kmers(text:str, k:int):
+    res = 0
+    rev_res = 1 << k - 1
+    for i in range(k-1):
+        res |= get_id_nucleotide(text[i])
+        rev_res |= get_id_rev(s[i])
+        res <<=2
+        rev_res >>=2
+    for i in range(k,len(text)):
+        res |= get_id_nucleotide(text[i])
+        rev_res |= get_id_rev(s[i])
+        yield min(res,rev_res)
+        res <<=2
+        rev_res >>=2
